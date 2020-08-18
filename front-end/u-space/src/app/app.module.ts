4650881ecc9a46bf5e257893dc16d4672ba67859
Erik1404 <kietnguyen1998@gmail.com>
2020-08-18 22:15:06 +0700

    list-kiet

--- front-end/u-space/src/app/app.module.ts
+++ front-end/u-space/src/app/app.module.ts
@@ -4,15 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FileFormatComponent } from './components/file-format/file-format.component';
+// -------------------------------------
+
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FileFormatComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
