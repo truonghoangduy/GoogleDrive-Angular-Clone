b607344e711263d1e00e458cd9747d051df49178
Erik1404 <kietnguyen1998@gmail.com>
2020-08-21 09:14:39 +0700

    sort

--- front-end/u-space/src/app/components/share-file/list-share/list-share.component.ts
+++ front-end/u-space/src/app/components/share-file/list-share/list-share.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {Folder} from '../../../models/folder.model';
 
-@Component({
-  selector: 'app-list-share',
-  templateUrl: './list-share.component.html',
-  styleUrls: ['./list-share.component.scss']
-})
-export class ListShareComponent implements OnInit {
 
-  constructor() { }
+
+  this.sortedData = data.sort((a, b) => {
+    const isAsc = sort.direction === 'asc';
+    switch (sort.active) {
+      case 'name': return compare(a.name, b.name, isAsc);
+      case 'calories': return compare(a.calories, b.calories, isAsc);
+      case 'fat': return compare(a.fat, b.fat, isAsc);
+      case 'carbs': return compare(a.carbs, b.carbs, isAsc);
+      case 'protein':return compare(a.protein, b.protein, isAsc);
+      default: return 0;
+    }
+  });
+
+
+
+function compare(a: number | string, b: number | string, isAsc: boolean) {
+return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+}
 
   ngOnInit(): void {
   }
 
-}
+
+
+
+
+
