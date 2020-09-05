8374276a63859e365a72c86eb5743eb0e1f5ac4e
truonghoangduy <truonghoangduy03111999@gmail.com>
2020-09-05 13:43:52 +0700

    conflic

--- backend/src/router/dowloadFile.ts
+++ backend/src/router/dowloadFile.ts
@@ -5,6 +5,7 @@ import path = require("path");
 import { HTTP_CODE } from '../models/HTTPCODE';
 
 const router = express.Router();
+const pathToWareHouse = path.join(__dirname, "..", "..", "warehouse")
 
 function nullChecker(key): boolean {
     if (key == undefined) {
@@ -17,7 +18,8 @@ router.get("", async (req, res) => {
     const { requestfile, uuid } = req.headers
     if (!nullChecker(requestfile) && !nullChecker(uuid)) {
         try {
-            var pathToWareHouse = path.join(__dirname, "..", "..", "warehouse")
+
+            // TODO CHECK AUTH
             var requestedPath = path.join(pathToWareHouse, <string>uuid, <string>requestfile)
             if (await fileInfo.fileExistFromRoot(requestedPath)) {
                 res.sendFile(requestedPath)
@@ -29,6 +31,38 @@ router.get("", async (req, res) => {
         }
     } else {
         res.send("File not exits").status(HTTP_CODE.BAD_REQUEST)
+    }
+});
+
+/**
+ * 
+ * @api {GET} /file/share Get user data
+ * @apiName GET SHARE FORM KEY
+ * 
+ * @apiHeader  {String} uid Logged-in user id
+ *             {requestfile} path -> to File
+ *             {sharkey} shareKey from the owner
+ * @apiRespone HTTP_CODE.BAD_REQUEST 400 missing Key
+ *             HTTP_CODE.INTERNAL_SERVER_ERROR I/O write Error
+ * */
+router.get("/share", async (req, res) => {
+
+    const { requestfile, uuid, sharekey } = req.headers
+    if (!nullChecker(requestfile) && !nullChecker(uuid) && !nullChecker(sharekey)) {
+        try {
+
+            // TODO CHECK AUTH
+            var requestedPath = path.join(pathToWareHouse, <string>uuid, <string>requestfile)
+            if (await fileInfo.fileExistFromRoot(requestedPath)) {
+                res.sendFile(requestedPath)
+            } else {
+                res.send("File not exits").status(HTTP_CODE.BAD_REQUEST)
+            }
+        } catch (error) {
+            res.status(HTTP_CODE.INTERNAL_SERVER_ERROR).send(HTTP_CODE.INTERNAL_SERVER_ERROR)
+        }
+    } else {
+        res.send("BAD_REQUEST").status(HTTP_CODE.BAD_REQUEST)
     }
 });
 export = router
