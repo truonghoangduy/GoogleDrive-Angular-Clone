a565d7886ff39a89dd7cb44df064277449c790d3
truonghoangduy <truonghoangduy03111999@gmail.com>
2020-09-07 15:18:26 +0700

    call backend API

--- front-end/MyApp/src/app/services/api.service.ts
+++ front-end/MyApp/src/app/services/api.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRoute, RouterLinkActive } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -8,9 +9,24 @@ export class ApiService {
 
   public root = "http://localhost:4200";
   mainScreenDection = false
-  constructor(public router:ActivatedRoute){
+  constructor(public router:ActivatedRoute,private http:HttpClient){
     this.router.data.subscribe((route)=>{
       route
     });
 
-  }}
+  }
+
+  removeFolder(){
+    // UUID/A
+              //B
+              //C
+
+    //  api brose UUID/A  => B C
+
+    // api bin UUID/A/B
+
+        //  api brose UUID/A  => C
+
+  }
+
+}
