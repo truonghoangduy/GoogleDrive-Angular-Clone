a3714becf1331188ea8337a286af186069939fe7
truonghoangduy <truonghoangduy03111999@gmail.com>
2020-09-07 15:20:12 +0700

    main-page drive snapshot from breadCrumb

--- front-end/MyApp/src/app/pages/main-page/main-page.component.ts
+++ front-end/MyApp/src/app/pages/main-page/main-page.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit,HostListener } from '@angular/core';
+import { Component, OnInit,HostListener, ViewChild } from '@angular/core';
 import {FolderService} from '../../services/folder.service';
 import { Folder } from 'src/app/models/folder.model';
+import { BreadcrumbService } from 'src/app/services/breadcrumb/breadcrumb.service';
+import { MatMenuTrigger } from '@angular/material/menu';
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
@@ -8,7 +10,20 @@ import { Folder } from 'src/app/models/folder.model';
 })
 export class MainPageComponent implements OnInit {
   // db :Array<Folder>=[];
-  constructor(public folerService: FolderService) { }
+  @ViewChild(MatMenuTrigger) contextMenu: MatMenuTrigger;
+
+  files:Array<String>;
+  folders:Array<String>;
+  contextMenuPosition = { x: '0px', y: '0px' };
+
+  constructor(public folerService: FolderService,public breadCrumbServices:BreadcrumbService) {
+    this.breadCrumbServices.currentViewFolder.subscribe((data)=>{
+      console.log("Change"),
+      console.log(data)
+      this.files = [...data.files],
+      this.folders = [...data.folders]
+    })
+   }
 
   screenWidth:number;
   @HostListener('window:resize', ['$event'])
@@ -18,6 +33,30 @@ export class MainPageComponent implements OnInit {
 
   ngOnInit(): void {
     this.getScreenSize(); 
+    this.breadCrumbServices.clearnBreadCrum();
+    this.breadCrumbServices.requestPath("admin") // UUID of someone
  
+  }
+
+  onContextMenu(event: MouseEvent, item) {
+    event.preventDefault();
+    this.contextMenuPosition.x = event.clientX + 'px';
+    this.contextMenuPosition.y = event.clientY + 'px';
+    this.contextMenu.menuData = { 'item': item };
+    this.contextMenu.menu.focusFirstItem('mouse');
+    this.contextMenu.openMenu();
+  }
+
+  onFolderClick(folderName:string){
+    // console.log(this.breadCrumbServices.currentPath + folderName);
+    this.breadCrumbServices.requestPath(this.breadCrumbServices.currentPath + "/"+folderName)
+
+  }
+  onContextMenuAction(item){
+    console.log(item)
+  }
+
+  onDelete(){
+    
   }
 }
