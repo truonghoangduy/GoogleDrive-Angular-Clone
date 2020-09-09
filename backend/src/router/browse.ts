7e0f787d3f4b62d0eab1ef9ffc598ef8ce6cabfc
Ferntran <ferntran0214@gmail.com>
2020-09-09 15:44:21 +0700

    update API

--- backend/src/router/browse.ts
+++ backend/src/router/browse.ts
@@ -32,6 +32,30 @@ router.post('/', async (res, resp) => {
         resp.send(err);
     }
 
+});
+
+router.post('/browse/share', async (res, resp) => {
+    const { currentDirectory } = res.body;
+    try {
+        let current = await fs.readdir(evn.environment.warehouse + "/" + currentDirectory)
+        let files = [];
+        let folders = [];
+        for (let i = 0; i < current.length; i++) {
+            if (current[i].includes('.')) {
+                files.push(current[i]);
+            } else {
+                folders.push(current[i])
+            }
+        }
+        resp.send({
+            requestPath:currentDirectory,
+            files,
+            folders
+        })
+    } catch (err) {
+        resp.send(err);
+    }
+
 })
 
 
--- backend/src/ults/wirteFile.ts
+++ backend/src/ults/wirteFile.ts
@@ -9,7 +9,7 @@ export function writeFileToDir(uuidName:string,data,directory:string){
     fs.ensureDir(evn.environment.warehouse+'/'+directory).then(()=>{ // If dicrectory not exits then created and append the file
         fs.writeFile(evn.environment.warehouse+'/'+uuidName,data).then(async (vaule)=>{
             console.log(`Done upload : ${uuidName}`)
-            await thumbnail.generateThumbnail(path.join(pathToWareHouse,uuidName))
+            // await thumbnail.generateThumbnail(path.join(pathToWareHouse,uuidName))
         })
     })
 }
